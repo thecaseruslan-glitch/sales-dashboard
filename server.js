b04d72a30534dbe5061f244a192addee24fbbdd2
thecaseruslan-glitch <the.case.ruslan@gmail.com>
2026-04-28 14:17:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+const express = require('express');
 const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
 });
